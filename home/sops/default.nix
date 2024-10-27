4ee19ab6f00421606804319d6bd939cdc2443657
harvey <nqphu1998@gmail.com>
2024-10-28 03:07:52 +0700

    fix: update secrects

--- home/sops/default.nix
+++ home/sops/default.nix
@@ -10,4 +10,14 @@
     # defaultSecretsMountPoint = "/run/user/1000/secrets.d";
   };
   sops.secrets."ssh/github/default" = {};
+  sops.secrets."ssh/github/charlie" = {};
+  sops.secrets."ssh/github/nqphu" = {};
+  sops.secrets."ssh/github/baron" = {};
+  sops.secrets."ssh/github/troy" = {};
+  sops.secrets."ssh/github/tim" = {};
+
+  sops.secrets."ssh/gitlab/harvey" = {};
+  sops.secrets."ssh/gitlab/keynold" = {};
+  sops.secrets."ssh/gitlab/marcus" = {};
+  sops.secrets."ssh/gitlab/zane" = {};
 }
--- home/ssh.nix
+++ home/ssh.nix
@@ -12,57 +12,67 @@
       github = {
         host = "github.com";
         hostname = "github.com";
+        identitiesOnly = true;
         identityFile = config.sops.secrets."ssh/github/default".path;
       };
       github-charlie = {
         host = "github.com-charlie";
         hostname = "github.com";
-        identityFile = "~/.ssh/id_rsa_charlie";
-      };
-      gitlab-harvey = {
-        host = "gitlab.com-harvey";
-        hostname = "gitlab.com";
-        identityFile = "~/.ssh/harvey-gitlab";
-      };
-      gitlab-keynold = {
-        host = "gitlab.com-keynold";
-        hostname = "gitlab.com";
-        identityFile = "~/.ssh/keynold-gitlab";
-      };
-      gitlab-marcus = {
-        host = "gitlab.com-marcus";
-        hostname = "gitlab.com";
-        identityFile = "~/.ssh/marcus-gitlab";
-      };
-      gitlab-zane = {
-        host = "gitlab.com-zane";
-        hostname = "gitlab.com";
-        identityFile = "~/.ssh/zane-gitlab";
+        identitiesOnly = true;
+        identityFile = config.sops.secrets."ssh/github/charlie".path;
       };
       github-nqp = {
         host = "github.com-nqp";
         hostname = "github.com";
-        identityFile = "~/.ssh/nqp-github";
+        identitiesOnly = true;
+        identityFile = config.sops.secrets."ssh/github/nqphu".path;
+      };
+      github-baron = {
+        host = "github.com-baron";
+        hostname = "github.com";
+        identitiesOnly = true;
+        identityFile = config.sops.secrets."ssh/github/baron".path;
+      };
+      github-troy = {
+        host = "github.com-troy";
+        hostname = "github.com";
+        identitiesOnly = true;
+        identityFile = config.sops.secrets."ssh/github/troy".path;
+      };
+      github-tim = {
+        host = "github.com-tim";
+        hostname = "github.com";
+        identitiesOnly = true;
+        identityFile = config.sops.secrets."ssh/github/tim".path;
+      };
+      gitlab-harvey = {
+        host = "gitlab.com-harvey";
+        hostname = "gitlab.com";
+        identitiesOnly = true;
+        identityFile = config.sops.secrets."ssh/gitlab/harvey".path;
+      };
+      gitlab-keynold = {
+        host = "gitlab.com-keynold";
+        hostname = "gitlab.com";
+        identitiesOnly = true;
+        identityFile = config.sops.secrets."ssh/gitlab/keynold".path;
+      };
+      gitlab-marcus = {
+        host = "gitlab.com-marcus";
+        hostname = "gitlab.com";
+        identitiesOnly = true;
+        identityFile = config.sops.secrets."ssh/gitlab/marcus".path;
+      };
+      gitlab-zane = {
+        host = "gitlab.com-zane";
+        hostname = "gitlab.com";
+        identitiesOnly = true;
+        identityFile = config.sops.secrets."ssh/gitlab/zane".path;
       };
       mynixos-nqp = {
         host = "git.mynixos.com-nqp";
         hostname = "git.mynixos.com";
         identityFile = "~/.ssh/nqphu-mynixos";
-      };
-      github-baron = {
-        host = "github.com-baron";
-        hostname = "github.com";
-        identityFile = "~/.ssh/baron-github";
-      };
-      github-troy = {
-        host = "github.com-troy";
-        hostname = "github.com";
-        identityFile = "~/.ssh/troy-github";
-      };
-      github-tim = {
-        host = "github.com-tim";
-        hostname = "github.com";
-        identityFile = "~/.ssh/tim-github";
       };
       bitbucket-troy = {
         host = "bitbucket.org-troy";
